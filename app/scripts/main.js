5fac6ddbd8f4bc75afc99af3e333e473a54aa0c4
Công Nguyễn <nhc96@HC-MacPro.local>
2022-06-13 13:51:59 +0700

    init source code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,15 @@
 const menuToggle = document.querySelector(".header-toggle");
 const menuHeader = document.querySelector(".header-menu");
-const expandClass = "is-expand";
 menuToggle.addEventListener("click", function () {
-  menuHeader.classList.add(expandClass);
+  menuHeader.classList.add("is-expand");
 });
 
-window.addEventListener("click", function (e) {
-  if (!menuHeader.contains(e.target) && !e.target.matches(".header-toggle")) {
-    menuHeader.classList.remove(expandClass);
+document.body.addEventListener("click", function (event) {
+  if (
+    !menuHeader.contains(event.target) &&
+    !event.target.matches(".header-toggle")
+  ) {
+    menuHeader.classList.remove("is-expand");
   }
 });
 
